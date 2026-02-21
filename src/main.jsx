354c6dc06965223b7c2047aaffa22746c2dcac03
scale <admin@soma.kernel>
2026-02-21 07:50:23 +0100

    feat:re-enable vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ResearchTerminal from './terminal'
 import './index.css'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
